4af9fad286c87233037d524874979a0695590c49
149203 <149203@gmail.com>
2018-06-28 11:03:42 -0700

    TODO: graphql query in template page, passing topic, order, and type from context

--- gatsby-node.js
+++ gatsby-node.js
@@ -34,29 +34,6 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
         }
       }
       
-      all_popular_posts: allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___rating] }
-        limit: 1000        
-      ) {
-        edges {
-          node {
-            fields {
-               slug
-            }
-            html
-            id
-            frontmatter {
-              title
-              date
-              topic
-              type
-              rating              
-              draft
-            }
-          }
-        }
-      }
-      
     }
         `
        ).then(result => {
@@ -66,11 +43,8 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
           }
 
           const all_recent_posts = result.data.all_recent_posts.edges;
-          const all_popular_posts = result.data.all_popular_posts.edges;
           create_blog_posts(createPage, all_recent_posts);
-          create_tag_pages(createPage, all_recent_posts, 'recent');
-          create_tag_pages(createPage, all_popular_posts, 'popular');
-
+          create_tag_pages(createPage, all_recent_posts);
        })
       )
    })
@@ -78,57 +52,62 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
 
 function create_blog_posts(createPage, posts) {
    /* Create pages for each markdown file. */
-   const blog_post = path.resolve(`src/templates/blog-post.js`);
+   // Creates a page
+   // Sends the slug to the page template
+   // The page template queries the data by each slug it was sent
 
    _.each(posts, (post) => {
       const slug = post.node.fields.slug
-      console.log('slug: ', slug)
+      console.log('individual blog post slug: ', slug)
       createPage({
          path: slug,
-         component: blog_post,
+         component: path.resolve(`src/templates/blog-post.js`),
          context: {
-            slug: slug,
+            slug: slug, // sends the context of the slug so that the query on the template page can query for just that slug
          },
       })
    })
 }
 
-function create_tag_pages(createPage, posts, order) {
-   const blog_post = path.resolve(`src/templates/tags.js`); // TODO: change or make this template handle context
-   _.each(posts, (post) => {
+function create_tag_pages(createPage, posts) {
 
-      // TODO: make all topics and types uniq
-      // create an array of uniq topics, including "all"
-      // create an array of uniq types, including "posts"
+   const topics = _.uniq(_.map(posts, post => post.node.frontmatter.topic).concat([ 'all' ]))
+   const orders = [ 'recent', 'popular' ]
+   const types = _.uniq(_.map(posts, post => post.node.frontmatter.type).concat([ 'posts' ]))
 
-      // for each slug pass topic, order, and type into context property
-      // create a page for each topic-order-type unique combo and pass it context
+   _.forEach(topics, topic => {
+      _.forEach(orders, order => {
+         _.forEach(types, type => {
+            const slug = `/blog/${slugify(topic)}-${order}-${slugify(type)}/`
+            console.log(`I'm creating a slug that look like: ${slug}`)
 
-      console.log('post.node.frontmatter: ', post.node.frontmatter)
-      const frontmatter = post.node.frontmatter
-      const slug = `/blog/${slugify(frontmatter.topic)}-${order}-${slugify(frontmatter.type)}/`
-      console.log('slug: ', slug)
-      createPage({
-         path: slug,
-         component: blog_post, // TODO: CHANGE
-         context: {
-            slug: slug,
-            // pass topic, order, and type into here
-         },
+            createPage({
+               path: slug,
+               component: path.resolve(`src/templates/tags.js`),
+               context: {
+                  // pass stuff into here to have it appear in template query
+                  slug,
+                  topic,
+                  order,
+                  type,
+               },
+            })
+
+         })
       })
    })
+
 }
 
 function slugify(str) {
    if (str === 'video' || str === 'article') str += 's' // pluralize these types, not really necessary though
-   return str.toLowerCase().replace(/[^a-zA-Z0-9\s]/g, '').replace(/\s/g, '-')
+   return str.toLowerCase().replace(/[^a-z0-9\s_\-]/g, '').replace(/\s/g, '-')
 }
-
 
 /**
  * Create pages for tags
  */
-function createTagPages(createPage, edges) {
+/*function createTagPages(createPage, edges) {
    const tag_template = path.resolve(`src/templates/tags.js`);
    const posts = {};
 
@@ -163,7 +142,7 @@ function createTagPages(createPage, edges) {
          });
       }
    });
-}
+}*/
 
 exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
    const { createNodeField } = boundActionCreators
