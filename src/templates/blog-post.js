7a84bdcbcc34b6156e25cbd0630caf4412a109f0
149203 <149203@gmail.com>
2018-07-21 08:57:14 -0700

    attempt at featuredImage #1

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -13,7 +13,7 @@ class BlogPostTemplate extends React.Component {
       const frontmatter = post.frontmatter
       const title = frontmatter.title
       const excerpt = post.excerpt
-      const featuredImage = `` // TODO: use imageSharp to get image. See lengstorf.com repo
+      const featuredImage = frontmatter.featuredImage.childImageSharp.resize.src // TODO: use imageSharp to get image. See lengstorf.com repo
       const author = get(this.props, 'data.site.siteMetadata.author')
       const slug = `https://mikezetlow.com${this.props.location.pathname}`
 
@@ -38,24 +38,24 @@ class BlogPostTemplate extends React.Component {
        <div>
           <Helmet>
              <title>{title}</title>
-             <meta name="description" content={excerpt} />
-             <meta name="image" content={featuredImage} />
+             <meta name="description" content={excerpt}/>
+             <meta name="image" content={featuredImage}/>
 
              {/* OpenGraph tags */}
-             <meta property="og:url" content={slug} />
-             <meta property="og:type" content="article" />
-             <meta property="og:locale" content="en_US" />
-             <meta property="og:title" content={title} />
-             <meta property="og:description" content={excerpt} />
-             <meta property="og:image" content={featuredImage} />
-             <meta property="og:site_name" content={siteTitle} />
+             <meta property="og:url" content={slug}/>
+             <meta property="og:type" content="article"/>
+             <meta property="og:locale" content="en_US"/>
+             <meta property="og:title" content={title}/>
+             <meta property="og:description" content={excerpt}/>
+             <meta property="og:image" content={featuredImage}/>
+             <meta property="og:site_name" content={siteTitle}/>
 
              {/* Twitter Card tags */}
-             <meta name="twitter:card" content="summary_large_image" />
-             <meta name="twitter:creator" content={author} />
-             <meta name="twitter:title" content={title} />
-             <meta name="twitter:description" content={excerpt} />
-             <meta name="twitter:image" content={featuredImage} />
+             <meta name="twitter:card" content="summary_large_image"/>
+             <meta name="twitter:creator" content={author}/>
+             <meta name="twitter:title" content={title}/>
+             <meta name="twitter:description" content={excerpt}/>
+             <meta name="twitter:image" content={featuredImage}/>
 
           </Helmet>
           < h1> {frontmatter.title
@@ -105,22 +105,25 @@ export default BlogPostTemplate
 
 export const pageQuery = graphql`
     query BlogPostBySlug($slug: String!) {
-        site {
-            siteMetadata {
-                title
-                author
-            }
+    site {
+        siteMetadata {
+            title
+            author
         }
-        markdownRemark(fields: { slug: { eq: $slug } }) {
-            id
-            html
-            excerpt
-            frontmatter {
-                topic
-                title
-                date(formatString: "MMMM Do, YYYY")
-                featuredImage {
-                    relativePath
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+        id
+        html
+        excerpt
+        frontmatter {
+            topic
+            title
+            date(formatString: "MMMM Do, YYYY")
+            featuredImage {
+                childImageSharp {
+                    resize(width: 1200, height: 630, cropFocus: ENTROPY) {
+                        src
+                    }
                 }
             }
         }
